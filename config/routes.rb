3ed4b0e9eb7ccb7ba9756da55d532c442f9c6746
Saideep A V S <annadatha@gmail.com>
2009-10-18 20:33:23 +0530

    minor changes in routes and gitignore.

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root '/', :controller => "home"
-
+   map.root :controller => "home"
+   map.home '/', :controller => "home", :action => "index"
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
